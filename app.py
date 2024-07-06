78c77eef5b79b76ba419f21d1916fe349e40cd18
ahmed mustafa <Ahmed_Mustafa_1251@fci.helwan.edu.eg>
2024-07-06 19:59:49 +0300

    new changes

--- app.py
+++ app.py
@@ -53,3 +53,49 @@ submit = st.button("Generate")
 ## Final response
 if submit:
     st.write(getLLamaresponse(input_text, no_words, blog_style))
+
+########################################################
+###############################################################
+class BlogGenerator:
+    def __init__(self, model_id):
+        self.model_id = model_id
+        self.llm = CTransformers(model=self.model_id,
+                                 model_type='llama',
+                                 config={'max_new_tokens': 256,
+                                         'temperature': 0.01})
+
+    def generate_prompt(self, blog_style, input_text, no_words):
+        template = """
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+        """
+        prompt = PromptTemplate(input_variables=["blog_style", "input_text", 'no_words'],
+                                template=template)
+        return prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words)
+
+    def get_response(self, input_text, no_words, blog_style):
+        prompt = self.generate_prompt(blog_style, input_text, no_words)
+        response = self.llm(prompt)
+        print(response)
+        return response
+
+
+# Initialize the BlogGenerator with the model ID
+blog_generator = BlogGenerator("meta-llama/Meta-Llama-3-8B-Instruct")
+
+st.set_page_config(page_title="Generate Blogs", page_icon='🤖', layout='centered', initial_sidebar_state='collapsed')
+st.header("Generate Blogs 🤖")
+
+input_text = st.text_input("Enter the Blog Topic")
+col1, col2 = st.columns([5, 5])
+
+with col1:
+    no_words = st.text_input('No of Words')
+with col2:
+    blog_style = st.selectbox('Writing the blog for', ('Researchers', 'Data Scientist', 'Common People'), index=0)
+
+submit = st.button("Generate")
+
+if submit:
+    response = blog_generator.get_response(input_text, no_words, blog_style)
+    st.write(response)
